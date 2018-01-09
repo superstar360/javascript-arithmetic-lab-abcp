5aaf842582086267c5923da7691811f09c2c1fa3
Abel Sobmenso Dadbutiso <abelsobmenso@gmail.com>
2018-01-09 21:26:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function multiply(a, b){
 function divide(a, b){
   return a / b
 }
+
+function inc(a) {
+  return a += 1
+}
+
+function dec(a) {
+  return a -= 1
+}
+
+function makeInt(a) {
+  return 
+}
